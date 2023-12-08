682881c9e16873ee141b46b862d8706a92ad930a
Kevin <kevzhangz888@gmail.com>
2023-12-08 22:14:53 +0700

    update for netlify hosting

--- functions/api.cjs
+++ functions/api.cjs
@@ -4,15 +4,19 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 
 // Routes
-import authRoutes from './routes/auth.routes.js'
-import userRoutes from './routes/user.routes.js'
+import authRoutes from '../src/routes/auth.routes.js'
+import userRoutes from '../src/routes/user.routes.js'
 
 // setup process.env
 import dotenv from 'dotenv'
 dotenv.config();
 
+import serverless from 'serverless-http'
+
 const app = express();
 const port = process.env.PORT || 8000;
+
+const router = express.Router();
 
 mongoose
   .connect(process.env.DB, { useNewUrlParser: true })
@@ -20,6 +24,12 @@ mongoose
   .catch((err) => console.log(err));
 
 mongoose.Promise = global.Promise;
+
+router.get("/", (req, res) => {
+  res.json({
+    hello: "hi"
+  })
+})
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -30,9 +40,13 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use(cors())
 
-app.use('/', authRoutes);
-app.use('/', userRoutes);
+app.use('/.netlify/functions/api/', authRoutes);
+app.use('/.netlify/functions/api/', userRoutes);
+app.use('/.netlify/functions/api/', router);
 
-app.listen(port, () => {
-  console.log(`Server running on port http://127.0.0.1:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server running on port http://127.0.0.1:${port}`);
+// });
+
+module.exports = app;
+module.exports.handler = serverless(app);
